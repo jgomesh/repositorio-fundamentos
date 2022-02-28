d976a3da17590435ce771a2827ef910e8de55e46
João Otávio <joao.gomeshugill@gmail.com>
2022-02-28 18:44:31 -0300

    novos exercicios javascript

---
+++ Fundamentos/bloco-04-introducao-javascript/exercicio-1-javascript/script.js
@@ -0,0 +1,9 @@
+const myName = "João Otávio Gomes Hugill";
+const birthCity = "Palhoça - SC";
+let birthYear = 1999;
+birthYear = 2030;
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
