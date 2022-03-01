788aae0a0a60367a8127425f2afa4ec51cf60131
João Otávio <joao.gomeshugill@gmail.com>
2022-03-01 04:55:26 -0300

    exercicio else if

---
+++ Fundamentos/bloco-04-introducao-javascript/exercicio-3-else-e-if/script.js
@@ -0,0 +1,11 @@
+const notaDesafio = 100;
+
+if(notaDesafio >= 80){
+    console.log("Parabéns, você foi aprovado!")
+}
+else if (notaDesafio < 80 && notaDesafio >= 60){
+    console.log("Você está na nossa lista de espera.")
+}
+else {
+    console.log("Você foi reprovado.")
+}
