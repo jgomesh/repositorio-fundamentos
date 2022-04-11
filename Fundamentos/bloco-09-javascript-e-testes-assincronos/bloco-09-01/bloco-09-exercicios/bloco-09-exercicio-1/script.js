ada19bea0f40a70d4fb1e83c9707302974bf523b
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-04-11 19:53:17 -0300

    Terminando o exercicio 51

--- Fundamentos/bloco-09-javascript-e-testes-assincronos/bloco-09-01/bloco-09-exercicios/bloco-09-exercicio-1/script.js
+++ Fundamentos/bloco-09-javascript-e-testes-assincronos/bloco-09-01/bloco-09-exercicios/bloco-09-exercicio-1/script.js
@@ -1,55 +1,55 @@
-// // // REQUISITO 1
-const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+// // // // REQUISITO 1
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
-const mars = {
-  name: "Mars",
-  distanceFromSun: {
-    value: 227900000,
-    measurementUnit: "kilometers",
-  },
-};
+// const mars = {
+//   name: "Mars",
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: "kilometers",
+//   },
+// };
 
-const venus = {
-  name: "Venus",
-  distanceFromSun: {
-    value: 108200000,
-    measurementUnit: "kilometers",
-  },
-};
+// const venus = {
+//   name: "Venus",
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: "kilometers",
+//   },
+// };
 
-const jupiter = {
-  name: "Jupiter",
-  distanceFromSun: {
-    value: 778500000,
-    measurementUnit: "kilometers",
-  },
-};
+// const jupiter = {
+//   name: "Jupiter",
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: "kilometers",
+//   },
+// };
 
-console.log(planetDistanceFromSun(mars)); // A 1º
-console.log(planetDistanceFromSun(venus)); // B 2º
-console.log(planetDistanceFromSun(jupiter)); // C 3º
+// console.log(planetDistanceFromSun(mars)); // A 1º
+// console.log(planetDistanceFromSun(venus)); // B 2º
+// console.log(planetDistanceFromSun(jupiter)); // C 3º
 
-// // // REQUISITO 2
+// // // // REQUISITO 2
 
-console.log(planetDistanceFromSun(mars)); // A 1º
-setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B 3º
-setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C 2º
+// console.log(planetDistanceFromSun(mars)); // A 1º
+// setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B 3º
+// setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C 2º
 
 
-// // REQUISITO 3
-const getPlanet = () => {
-  const mars = {
-    name: "Mars",
-    distanceFromSun: {
-      value: 227900000,
-      measurementUnit: "kilometers",
-    },
-  };
-  console.log("Returned planet: ", mars);
-};
+// // // REQUISITO 3
+// const getPlanet = () => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   console.log("Returned planet: ", mars);
+// };
 
-setTimeout(getPlanet, 4000); 
+// setTimeout(getPlanet, 4000); 
 
 // REQUISITO 4
 const messageDelay = () => Math.floor(Math.random() * 5000);
@@ -61,8 +61,22 @@ const getMarsTemperature = () => {
 
 // crie a função sendMarsTemperature abaixo
 
-const sendMarsTemperature = () => {
-  setTimeout(() => console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`), messageDelay());
+const sendMarsTemperature = (temperatureTypeFunc = () => console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`)) => {
+  temperatureTypeFunc(getMarsTemperature());
 }
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+// REQUISITO 5
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// definição da função sendMarsTemperature...
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
