34f41876232ac88e150843889db90c06c2256617
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-04-11 19:19:48 -0300

    Requisito 3

--- Fundamentos/bloco-09-javascript-e-testes-assincronos/bloco-09-01/bloco-09-exercicios/bloco-09-exercicio-1/script.js
+++ Fundamentos/bloco-09-javascript-e-testes-assincronos/bloco-09-01/bloco-09-exercicios/bloco-09-exercicio-1/script.js
@@ -1,7 +1,7 @@
-// REQUISITO 1
+// // REQUISITO 1
 
-const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
 const mars = {
   name: "Mars",
@@ -31,8 +31,23 @@ console.log(planetDistanceFromSun(mars)); // A 1º
 console.log(planetDistanceFromSun(venus)); // B 2º
 console.log(planetDistanceFromSun(jupiter)); // C 3º
 
-// REQUISITO 2
+// // REQUISITO 2
 
 console.log(planetDistanceFromSun(mars)); // A 1º
 setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B 3º
 setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C 2º
+
+
+// REQUISITO 3
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+};
+
+setTimeout(getPlanet, 4000); 
