c02eff46f3b899856aedfde22fe97b094c43d0ad
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-04-11 19:13:43 -0300

    Inserindo exercicios 1 e 2

---
+++ Fundamentos/bloco-09-javascript-e-testes-assincronos/bloco-09-01/bloco-09-exercicios/bloco-09-exercicio-1/script.js
@@ -0,0 +1,38 @@
+// REQUISITO 1
+
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A 1º
+console.log(planetDistanceFromSun(venus)); // B 2º
+console.log(planetDistanceFromSun(jupiter)); // C 3º
+
+// REQUISITO 2
+
+console.log(planetDistanceFromSun(mars)); // A 1º
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B 3º
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C 2º
