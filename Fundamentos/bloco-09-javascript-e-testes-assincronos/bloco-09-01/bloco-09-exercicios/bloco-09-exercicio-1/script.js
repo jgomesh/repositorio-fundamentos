bf4ef6392485da61e1ebd0cc67ed54e970ea73ae
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-04-12 13:01:46 -0300

    Terminando o requisito 8

--- Fundamentos/bloco-09-javascript-e-testes-assincronos/bloco-09-01/bloco-09-exercicios/bloco-09-exercicio-1/script.js
+++ Fundamentos/bloco-09-javascript-e-testes-assincronos/bloco-09-01/bloco-09-exercicios/bloco-09-exercicio-1/script.js
@@ -1,112 +1,151 @@
-// // // // // REQUISITO 1
-// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
+// // // // REQUISITO 1
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
-// const mars = {
-//   name: "Mars",
-//   distanceFromSun: {
-//     value: 227900000,
-//     measurementUnit: "kilometers",
-//   },
-// };
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// const venus = {
-//   name: "Venus",
-//   distanceFromSun: {
-//     value: 108200000,
-//     measurementUnit: "kilometers",
-//   },
-// };
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// const jupiter = {
-//   name: "Jupiter",
-//   distanceFromSun: {
-//     value: 778500000,
-//     measurementUnit: "kilometers",
-//   },
-// };
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// console.log(planetDistanceFromSun(mars)); // A 1º
-// console.log(planetDistanceFromSun(venus)); // B 2º
-// console.log(planetDistanceFromSun(jupiter)); // C 3º
+console.log(planetDistanceFromSun(mars)); // A 1º
+console.log(planetDistanceFromSun(venus)); // B 2º
+console.log(planetDistanceFromSun(jupiter)); // C 3º
 
-// // // // // REQUISITO 2
+// // // // REQUISITO 2
 
-// console.log(planetDistanceFromSun(mars)); // A 1º
-// setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B 3º
-// setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C 2º
-
-
-// // // // REQUISITO 3
-// const getPlanet = () => {
-//   const mars = {
-//     name: "Mars",
-//     distanceFromSun: {
-//       value: 227900000,
-//       measurementUnit: "kilometers",
-//     },
-//   };
-//   console.log("Returned planet: ", mars);
-// };
-
-// setTimeout(getPlanet, 4000); 
-
-// // REQUISITO 4
-// const messageDelay = () => Math.floor(Math.random() * 5000);
-
-// const getMarsTemperature = () => {
-//   const maxTemperature = 58;
-//   return Math.floor(Math.random() * maxTemperature);
-// };
-
-// // crie a função sendMarsTemperature abaixo
-
-// const sendMarsTemperature = (temperatureTypeFunc = () => console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`), errorFunc) => {
-//   const didOperationSucceed = Math.random() >= 0.6;
-//   if(didOperationSucceed) {
-//     temperatureTypeFunc(getMarsTemperature());
-//   } else {
-//     errorFunc('Robot is busy');
-//   }
-// }
-
-// sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
-
-// // REQUISITO 5
-// const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
-
-// const temperatureInFahrenheit = (temperature) =>
-//   console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
-
-// const greet = (temperature) =>
-//   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
-
-// // definição da função sendMarsTemperature...
-
-// sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-// sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
-
-// // REQUISITO 6
-
-// const handleError = (errorReason) =>
-//   console.log(`Error getting temperature: ${errorReason}`);
-
-// // definição da função sendMarsTemperature...
-
-// // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-// sendMarsTemperature(temperatureInFahrenheit, handleError);
-
-// // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-// sendMarsTemperature(greet, handleError);
+console.log(planetDistanceFromSun(mars)); // A 1º
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B 3º
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C 2º
 
 
-const teste = (string) => {
-  return string
+// // // REQUISITO 3
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+};
+
+setTimeout(getPlanet, 4000); 
+
+// REQUISITO 4
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = (temperatureTypeFunc = () => console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`), errorFunc) => {
+  const didOperationSucceed = Math.random() >= 0.6;
+  if(didOperationSucceed) {
+    temperatureTypeFunc(getMarsTemperature());
+  } else {
+    errorFunc('Robot is busy');
+  }
 }
 
-const uppercase = (str, callback) => {
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+// REQUISITO 5
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// definição da função sendMarsTemperature...
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+
+// // // REQUISITO 6
+
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
+// definição da função sendMarsTemperature...
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
+
+
+// // REQUISITO 7
+
+
+const uppercase = async(str, callback) => {
   setTimeout(() => {
    callback(str.toUpperCase());
   }, 500);
 };
-console.log(teste('teste'.toUpperCase()))
+
+// REQUISITO 8
+
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
+  },
+];
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find(({name}) => name === filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+    const pokemon = pokemons.find(({name}) => name === filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(messageFromProfOak);
+  }, 2000);
+}
+
+getPokemonDetails('Charmander', console.log);
+
+module.exports = {
+  getPokemonDetails,
+};
--- Fundamentos/bloco-09-javascript-e-testes-assincronos/bloco-09-01/bloco-09-exercicios/bloco-09-exercicio-1/script.test.js
+++ Fundamentos/bloco-09-javascript-e-testes-assincronos/bloco-09-01/bloco-09-exercicios/bloco-09-exercicio-1/script.test.js
@@ -1,16 +1,64 @@
-// REQUISITO 7
+// // REQUISITO 7
 
-const teste = (string) => {
-  return string
-}
-
-const uppercase = (str, callback) => {
+const uppercase = async(str, callback) => {
   setTimeout(() => {
-   return callback(str.toUpperCase());
+   callback(str);
   }, 500);
 };
 
-test('Não deveria passar!', () => {
-      expect([uppercase('teste eu', teste)]).toContain('TESTE EU');
-      console.log('Deveria falhar!');
+describe("A função uppercase funciona como deveria?", () => {
+  it('A função uppercase retorna uma string em uppercase', (done) => {
+    uppercase("aBobORa", (result) => {
+      try {
+        expect(result).toBe("ABOBORa");
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  })
+
+});
+
+// REQUISITO 8
+// loginValidation.test.js
+const { expect } = require("@jest/globals");
+const {
+  getPokemonDetails,
+} = require("./script.js");
+
+describe("A função getPokemon funciona como deveria?", () => {
+    it('A função getPokemonDetails retorna os dados do pokemon', (done) => {
+      getPokemonDetails("Charmander", (result) => {
+        try {
+          expect(result).toBe("Olá, seu pokémon é o Charmander, o tipo dele é Fire e a habilidade principal dele é Ember");
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    })
+  
+    it("retorna erro quando procuramos um pokemon que não existe no banco de dados", (done) => {
+      getPokemonDetails("Adadada", (result) => {
+        try {
+          expect(result).toEqual(new Error("Não temos esse pokémon para você :("));
+          done();
+        } catch (error) {
+          done(error)
+        }
+      });
+    });
+  });
+// // REQUISITO 10
+beforeEach(() => console.log('1 - beforeEach')); //1 - 4
+afterEach(() => console.log('1 - afterEach')); //3 - 8
+
+test('', () => console.log('1 - test')); //2
+
+describe('Scoped / Nested block', () => {  
+  beforeEach(() => console.log('2 - beforeEach'));// 5
+  // afterEach(() => console.log('2 - afterEach')); 7
+
+  test('', () => console.log('2 - test')); // 6
 });
