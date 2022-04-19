0c3d193da320ac0227ed927fcaecd9071339fb5c
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-04-19 11:22:46 -0300

    Att

--- Fundamentos/projetos-pessoais/tentativaV2/assets/js/script.js
+++ Fundamentos/projetos-pessoais/tentativaV2/assets/js/script.js
@@ -108,3 +108,37 @@ loop: true,
     el: ".swiper-pagination",
   },
 });
+
+// DARK LIGHT THEME
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+// PREVIOUSLY SELECTED TOPIC (IF USER SELECTED
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme calss
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+// We validate if the user previusly chose a topic
+
+if (selectedTheme) {
+  // activate or deactivate verification
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// ACTIVATE BOYYYY
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+})
