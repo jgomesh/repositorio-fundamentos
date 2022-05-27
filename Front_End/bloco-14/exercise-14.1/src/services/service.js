54bbd2534ccfa62e2f6afd0f66c89cf09adc1f4a
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-05-27 13:21:57 -0300

    Fazendo os primeiros testes'

---
+++ Front_End/bloco-14/exercise-14.1/src/services/service.js
@@ -0,0 +1,9 @@
+const randomHundredNumber = () => {
+  return Math.floor(100* Math.random())
+} 
+
+const toUpperCase = (string) => {
+  return string.toUpperCase();
+}
+
+module.exports = { randomHundredNumber };
